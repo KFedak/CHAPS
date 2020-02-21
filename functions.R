0dcab46d0ae97f6dc16081a4183a2d91facc8e21
KFedak <Kristen.Fedak@colostate.edu>
2020-02-21 11:17:05 -0700

    initial commit

---
+++ functions.R
@@ -0,0 +1,23 @@
+
+#_______base LF model (no exposure metrics)______________________________
+mod_base <- function(df){
+    lm(value ~ age_months + height_cm^2 + weight_kg + sex + race_cat + asthma_ever, 
+       data = df)
+}
+#________________________________________________________________________
+
+
+#_______lifetime average exposure LF model______________________________
+mod_lifetime_exp <- function(df){
+  lm(value ~ exp_avg_lifetime + age_months + height_cm^2 + weight_kg + sex + race_cat + asthma_ever,
+     data = df)
+}
+#________________________________________________________________________
+
+
+#_______prior year average exposure LF model______________________________
+mod_lastyear_exp <- function(df){
+  lm(value ~ exp_avg_lastyear + age_months + height_cm^2 + weight_kg + sex + race_cat + asthma_ever,
+     data = df)
+}
+#________________________________________________________________________
